87c51a06b063517157cd3d0149acc46af37c90e3
Kazuya Hosokawa <rp.kazuya.hosokawa@gmail.com>
2019-11-01 01:11:14 +0900

    add fish setup scripts

---
+++ setup/mac/fish.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+brew install fish
+
+echo /usr/local/bin/fish | sudo tee -a /etc/shells
+chsh -s /usr/local/bin/fish
+
+# plugin manager
+# use fisher
+curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+
+# install powerline font
+git clone https://github.com/powerline/fonts.git --depth=1 ~/dotfiles/tmp/fonts
+~/dotfiles/tmp/fonts/install.sh
+rm -rf ~/dotfiles/tmp/fonts
+
+# install fzf
+brew install fzf
+
+# install ghq
+brew install ghq
+
+# add fisher plugin
+./fish_add_plugin.sh
---
+++ setup/mac/fish_add_plugin.sh
@@ -0,0 +1,13 @@
+#!/usr/local/bin/fish
+
+# install on my fish theme bobthefish
+fisher add oh-my-fish/theme-bobthefish
+
+# z
+fisher add jethrokuan/z
+
+# fzf
+fisher add jethrokuan/fzf
+
+# ghq
+fisher add decors/fish-ghq
